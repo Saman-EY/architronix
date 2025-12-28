1ea72d3670a01c21df3722baa913571d442cab57
Saman <samanezzatabadi@yahoo.com>
2025-12-28 11:57:09 +0330

    maybe done

--- russian/src/components/ui/DetailsModal.jsx
+++ russian/src/components/ui/DetailsModal.jsx
@@ -5,6 +5,7 @@ import { setModalRdx } from "@/redux/others";
 import { useDispatch } from "react-redux";
 import toast from "react-hot-toast";
 import { publicApi } from "@/lib/api";
+import Image from "next/image";
 
 function DetailsModal() {
   const dispatch = useDispatch();
@@ -35,11 +36,12 @@ function DetailsModal() {
         email,
       };
 
-      await publicApi.post("/api/send-details", body); // or your real endpoint
+      await publicApi.post("/api/send-details", body);
 
       toast.success("Your details were submitted successfully ✅");
       localStorage.setItem(SUBMITTED_KEY, "true");
       dispatch(setModalRdx(false));
+      window.open("/brochure.pdf", "_blank");
 
       // optional reset
       setFullname("");
@@ -62,22 +64,38 @@ function DetailsModal() {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
-        className="fixed top-1/2  left-1/2 -translate-y-1/2 rounded-md -translate-x-1/2 w-full max-w-[90%] md:max-w-[500px]  bg-white z-[105] flex flex-col gap-5 p-5"
+        className="fixed top-1/2  left-1/2 -translate-y-1/2 rounded-md -translate-x-1/2 w-full max-w-[90%] md:max-w-[500px]  bg-slate-100 z-[105] flex flex-col gap-3 p-5"
       >
         <button
           onClick={() => dispatch(setModalRdx(false))}
-          className="bg-black text-white rounded-full size-7 flex items-center justify-center text-xs absolute right-5 hover:bg-black/80 transition-all"
+          className="bg-black text-white rounded-full size-7 flex items-center justify-center text-xs absolute right-2 top-2 hover:bg-black/80 transition-all"
         >
           X
         </button>
-        <h1 className="font-semibold text-xl text-center mb-5 mt-10">Submit Your Information To Proceed</h1>
+
+        <section className="">
+          <Image className="w-full object-cover h-[200px]" width={500} height={500} src={"/topFooter.JPG"} alt="" />
+
+          <div className="flex flex-col gap-3">
+            <h6 className="text-xl text-center mt-3 font-semibold">PAYMENT PLAN</h6>
+            <div>
+              <PaymentCard title={"DOWNPAYMENT"} price={"20%"} desc={"IMMEDIATE"} />
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <PaymentCard desc={"After 6 months from Booking Date"} price={"10%"} title={"1 INSTALLMENT"} />
+              <PaymentCard desc={"After 12 months from Booking Date"} price={"10%"} title={"2 INSTALLMENT"} />
+              <PaymentCard desc={"After 24 months from Booking Date"} price={"10%"} title={"3 INSTALLMENT"} />
+              <PaymentCard desc={"On project completion"} price={"50%"} title={"FINAL INSTALLMENT"} />
+            </div>
+          </div>
+        </section>
 
         <input
           value={fullname}
           onChange={(e) => setFullname(e.target.value)}
           type="text"
           placeholder="Ваше имя"
-          className="border border-gray-500 rounded-md p-2"
+          className="border border-gray-500 rounded-md p-2 text-base"
           onInput={(e) => {
             e.currentTarget.value = e.currentTarget.value.replace(/^\s+/, "");
           }}
@@ -87,7 +105,7 @@ function DetailsModal() {
           onChange={(e) => setPhone(e.target.value)}
           type="text"
           placeholder="Номер телефона"
-          className="border border-gray-500 rounded-md p-2"
+          className="border border-gray-500 rounded-md p-2 text-base"
           onInput={(e) => {
             let value = e.currentTarget.value;
 
@@ -107,7 +125,7 @@ function DetailsModal() {
           onChange={(e) => setEmail(e.target.value)}
           type="text"
           placeholder="E-Mail адрес"
-          className="border border-gray-500 rounded-md p-2"
+          className="border border-gray-500 rounded-md p-2 text-base"
           onInput={(e) => {
             e.currentTarget.value = e.currentTarget.value.replace(/^\s+/, "");
           }}
@@ -115,9 +133,9 @@ function DetailsModal() {
 
         <button
           onClick={handleSubmit}
-          className="px-6 py-2 text-white hover:bg-black/80 transition-all bg-black mt-auto rounded"
+          className="px-6 py-2 text-white hover:bg-black/80 transition-all text-sm md:text-base bg-black mt-auto rounded"
         >
-          Submit Details
+          Submit And Download Brochure
         </button>
       </motion.div>
     </section>
@@ -125,3 +143,13 @@ function DetailsModal() {
 }
 
 export default DetailsModal;
+
+const PaymentCard = ({ title, price, desc }) => {
+  return (
+    <div className="bg-white p-3 flex flex-col justify-center items-center">
+      <h6 className="text-3xl">{price}</h6>
+      <span className="text-sm">{title}</span>
+      <span className="text-xs text-slate-400">{desc}</span>
+    </div>
+  );
+};
